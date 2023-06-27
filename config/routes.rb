0b3fd9cc08df8e75fd0143a5962dd2cb04c1c3e0
fartun <fartunwario34@gmail.com>
2023-06-27 22:03:22 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:index]
-  resources :dog_houses, only: [:show]
+  get '/reviews', to: 'reviews#index'
+  get '/doghouses', to: 'dog_houses#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
